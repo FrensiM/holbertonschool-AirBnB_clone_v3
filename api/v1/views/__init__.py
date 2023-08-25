16b05d2f30a80c0b85b1c921d4a6543e7942b891
User <user@Users-MacBook.local>
2023-08-25 10:25:26 +0200

    add

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,10 +5,4 @@ from flask import Blueprint
 app_views = Blueprint('api', __name__, url_prefix='/api/v1')
 
 if app_views is not None:
-    from api.v1.views.users import *
-    from api.v1.views.amenities import *
-    from api.v1.views.cities import *
-    from api.v1.views.states import *
-    from api.v1.views.places import *
-    from api.v1.views.places_reviews import *
     from api.v1.views.index import *
