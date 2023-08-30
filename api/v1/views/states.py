f552fb02ad5ea5fd513ed947534aa745c05a0f88
User <user@Users-MacBook.local>
2023-08-30 11:02:13 +0200

    import

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,7 +2,7 @@
 """
 route for handling State objects and operations
 """
-from flask import jsonify, abort, request
+from flask import jsonify, abort, request, make_response
 from api.v1.views import app_views
 from models import storage
 from models.state import State
@@ -28,18 +28,15 @@ def state_create():
     create state route
     :return: newly created state obj
     """
-    state_json = request.get_json(silent=True)
-    if state_json is None:
+    if not request.get_json():
         abort(400, 'Not a JSON')
-    if "name" not in state_json:
+    if "name" not in request.get_json():
         abort(400, 'Missing name')
 
-    new_state = State(**state_json)
+    data = request.get_json()
+    new_state = State(**data)
     new_state.save()
-    resp = jsonify(new_state.to_json())
-    resp.status_code = 201
-
-    return resp
+    return make_response(jsonify(new_state.to_dict()), 201)
 
 
 @app_views.route("/states/<state_id>",  methods=["GET"], strict_slashes=False)
