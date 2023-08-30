e242e852af5411f419202dbdd9d87adbeda50974
User <user@Users-MacBook.local>
2023-08-30 13:04:18 +0200

    add

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -4,36 +4,42 @@ from models.user import User
 from models import storage
 from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
+from flasgger.utils import swag_from
 
 
-@app_views.route("/users", methods=['GET'], strict_slashes=False)
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
+@swag_from('documentation/user/all_users.yml')
 def get_users():
     """
-    Lists of users
+    Retrieves the list of all user objects
+    or a specific user
     """
     all_users = storage.all(User).values()
-    list_user = []
+    list_users = []
     for user in all_users:
-        list_user.append(user.to_dict())
-    return jsonify(list_user)
+        list_users.append(user.to_dict())
+    return jsonify(list_users)
 
-@app_views.route("/users/<user_id>", methods=['GET'],
-                 strict_slashes=False)
+
+@app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
+@swag_from('documentation/user/get_user.yml', methods=['GET'])
 def get_user(user_id):
-    """
-    retrievs a user
-    """
+    """ Retrieves an user """
     user = storage.get(User, user_id)
     if not user:
         abort(404)
 
     return jsonify(user.to_dict())
 
-@app_views.route("/user/<user_id", methods=['DELETE'])
+
+@app_views.route('/users/<user_id>', methods=['DELETE'],
+                 strict_slashes=False)
+@swag_from('documentation/user/delete_user.yml', methods=['DELETE'])
 def delete_user(user_id):
     """
-    Delete  user
+    Deletes a user Object
     """
+
     user = storage.get(User, user_id)
 
     if not user:
@@ -44,7 +50,9 @@ def delete_user(user_id):
 
     return make_response(jsonify({}), 200)
 
+
 @app_views.route('/users', methods=['POST'], strict_slashes=False)
+@swag_from('documentation/user/post_user.yml', methods=['POST'])
 def post_user():
     """
     Creates a user
@@ -64,6 +72,7 @@ def post_user():
 
 
 @app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
+@swag_from('documentation/user/put_user.yml', methods=['PUT'])
 def put_user(user_id):
     """
     Updates a user
